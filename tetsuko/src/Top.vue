9cd5f06f643191903dd67155873d3194e07fb9f5
kenjiskywalker <git@kenjiskywalker.org>
2017-04-22 16:33:59 +0900

    静的なページは行けそう

---
+++ tetsuko/src/Top.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="top">
+  aaaaaaaaaaaaああああああ<br>
+      <img src="./assets/text.png">
+      <img src="./assets/logo.png">
+      aaaaaaaaaaaa
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Hello World!'
+    }
+  },
+  methods: {
+    clear () {
+      this.msg = ''
+    }
+  }
+}
+</script>
+
+<style>
+#top {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #fff;
+  background-color: #fff;
+}
+</style>
--- tetsuko/src/main.js
+++ tetsuko/src/main.js
@@ -1,13 +1,19 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import BgMovie from './BgMovie'
+import Top from './Top'
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App }
+  el: '#bgmovie',
+  template: '<BgMovie/>',
+  components: { BgMovie }
+})
+
+new Vue({
+  el: '#top',
+  template: '<Top>',
+  components: { Top }
 })
