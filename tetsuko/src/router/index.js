6202a8bc114edfea2b1092df92aa5976af289fed
kenjiskywalker <git@kenjiskywalker.org>
2017-04-22 15:08:55 +0900

    re init

--- tetsuko/src/router/index.js
+++ tetsuko/src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Top from '@/components/Top'
-// import 'css/common.css'
+import Hello from '@/components/Hello'
 
 Vue.use(Router)
 
@@ -9,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'hoge',
-      component: Top
+      name: 'Hello',
+      component: Hello
     }
   ]
 })
